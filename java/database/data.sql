53fd9da87fe32c945bef846d417808e06b307142
Caitlin Nagelson <cnagelson@gmail.com>
2022-08-07 14:17:49 -0400

    PetPals ERD and database design

--- java/database/data.sql
+++ java/database/data.sql
@@ -4,3 +4,31 @@ INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULi
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
 COMMIT TRANSACTION;
+--
+----PET INSERTS--
+--
+--INSERT INTO pets(name, species, sex, birth_date, personality, is_fixed, has_vaccinations, size)
+--VALUES ('Junebug', 'dog', 'female', '2015-06-10', 'Loving and excitable but calms down quickly. June loves to be chased and loves to try to eat bees. She is not a fan of wrestling and does not like to be bullied.', true, true, 'small');
+--
+--INSERT INTO pets(name, species, sex, birth_date, personality, is_fixed, has_vaccinations, size)
+--VALUES ('Kona', 'dog', 'female', '2017-04-15', 'Kona is a total sweetheart and gets along with most breeds. She loves all type of play but is slightly territorial with chew toys', true, true, 'small');
+--
+--INSERT INTO pets(name, species, sex, birth_date, personality, is_fixed, has_vaccinations, size)
+--VALUES ('James Franco', 'dog', 'male', '2013-08-15', 'High-energy. Always loves a good chase and tummy rub. Very social - almost too co-dependent.', true, true, 'large');
+--
+----USER INSERTS--
+--INSERT INTO users(first_name, last_name, username, role, email_address, address, birth_date)
+--VALUES ('Caito', 'Nagelson', 'c80_grace', 'role_user', 'cnagelson@gmail.com', '2305 Carrie St, Pittsburgh PA, 15212', '1987-10-20');
+--
+--INSERT INTO users(first_name, last_name, username, role, email_address, address, birth_date)
+--VALUES ('Bradley', 'Mauger', 'bmauger', 'role_admin', 'bradleyamauger@gmail.com', '1917 Fairmount Avenue, Cincinnati OH, 45214', '03/10/1988');
+--
+----USER/PET INSERTS--
+--INSERT INTO user_pet (user_id, pet_id)
+--VALUES ((SELECT user_id FROM users WHERE first_name = 'Caito'), (SELECT pet_id from pets WHERE name = 'Junebug'));
+--
+--INSERT INTO user_pet (user_id, pet_id)
+--VALUES ((SELECT user_id FROM users WHERE first_name = 'Caito'), (SELECT pet_id from pets WHERE name = 'Kona'));
+--
+--INSERT INTO user_pet (user_id, pet_id)
+--VALUES ((SELECT user_id FROM users WHERE first_name = 'Bradley'), (SELECT pet_id from pets WHERE name = 'James Franco'));
--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,13 +1,74 @@
 BEGIN TRANSACTION;
+DROP TABLE IF EXISTS users, pets, playdate, pet_playdate, user_pet, messages;
 
-DROP TABLE IF EXISTS users;
+--TABLE CREATION--
 
-CREATE TABLE users (
-	user_id SERIAL,
-	username varchar(50) NOT NULL UNIQUE,
-	password_hash varchar(200) NOT NULL,
+CREATE TABLE users
+(
+	user_id serial,
+	first_name varchar(50) NOT NULL,
+	last_name varchar(50) NOT NULL,
+	username varchar(50) UNIQUE NOT NULL,
+	password_hash varchar(200),
 	role varchar(50) NOT NULL,
-	CONSTRAINT PK_user PRIMARY KEY (user_id)
+	email_address varchar(250) NOT NULL,
+	address varchar(500) NOT NULL,
+	birth_date date,
+	pickle_points int,
+
+	constraint pk_user PRIMARY KEY (user_id)
+);
+CREATE TABLE pets
+(
+	pet_id serial,
+	name varchar(50) NOT NULL,
+	species varchar(50) NOT NULL,
+	sex varchar(10),
+	birth_date date NOT NULL,
+	personality varchar(1500) NOT NULL,
+	is_fixed boolean NOT NULL,
+	has_vaccinations boolean NOT NULL,
+	size varchar (15),
+
+	constraint pk_pet PRIMARY KEY (pet_id)
+);
+CREATE TABLE playdate
+(
+	playdate_id serial,
+	location varchar(500) NOT NULL,
+	date_and_time timestamp NOT NULL,
+	details varchar(1000),
+	rating int,
+	status varchar(10),
+
+	constraint pk_playdate PRIMARY KEY (playdate_id)
+);
+CREATE TABLE pet_playdate
+(
+	pet_id int NOT NULL,
+	playdate_id int NOT NULL,
+
+	constraint pk_pet_playdate PRIMARY KEY (pet_id, playdate_id),
+	constraint fk_pet_playdate_pet_id FOREIGN KEY (pet_id) references pets (pet_id),
+	constraint fk_pet_playdate_playdate_id FOREIGN KEY (playdate_id) references playdate (playdate_id)
+);
+CREATE TABLE user_pet
+(
+	user_id int NOT NULL,
+	pet_id int NOT NULL,
+
+	constraint pk_user_pet PRIMARY KEY (user_id, pet_id),
+	constraint fk_user_pet_user_id FOREIGN KEY (user_id) references users (user_id),
+	constraint fk_user_pet_pet_id FOREIGN KEY (pet_id) references pets (pet_id)
+);
+CREATE TABLE messages
+(
+	message_id serial,
+	body varchar(1500),
+	user_id int,
+
+	constraint pk_message PRIMARY KEY (message_id),
+	constraint fk_user FOREIGN KEY (user_id) references users (user_id)
 );
 
 COMMIT TRANSACTION;
