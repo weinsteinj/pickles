15df764f753ce69a853dc4ecee3833920c39d9c0
Josh Weinstein <yamaotoko@gmail.com>
2022-08-08 12:03:08 -0400

    work at VUE add image logo, add NavBar, add comps: Login, PlayDate, Register

---
+++ vue/src/components/NavBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="nav-bar">
+      
+      <router-link v-bind:to="{name: 'home' }">
+          <img class="logo" src="@/assets/images/FullTurtle.png" alt="cute green turtle logo">
+      </router-link>
+      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+      <router-link v-bind:to="{ name: 'login' }" v-else>Login</router-link>
+
+      This is our persistent Navigation Bar!
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,12 +1,30 @@
 <template>
   <div class="home">
+    <div>PLAYDATE STUFF</div>
+    <div>
+    <login></login>
+    </div>
+  
     <h1>Home</h1>
     <p>You must be authenticated to see this</p>
   </div>
 </template>
 
 <script>
+import Login from '@/components/Login.vue'
+
 export default {
-  name: "home"
+  name: "home",
+  components: {
+    Login,
+  
+
+  }
 };
 </script>
+
+<style scoped>
+.home {
+  display: flex;
+}
+</style>
