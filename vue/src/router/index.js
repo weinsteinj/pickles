2b660270e2511b0577b0887b773e1a19f0c3e176
Nicholas Soilis <nicholassoilis@gmail.com>
2022-08-09 11:34:53 -0400

    Pet Register Page WIP

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -5,6 +5,7 @@ import Login from '../views/Login.vue'
 import Logout from '../views/Logout.vue'
 import Register from '../views/Register.vue'
 import store from '../store/index'
+import petRegister from '../views/Register-Pet.vue'
 
 Vue.use(Router)
 
@@ -49,6 +50,14 @@ const router = new Router({
       path: "/register",
       name: "register",
       component: Register,
+      meta: {
+        requiresAuth: false
+      }
+    },
+    {
+      path: "/user/:user_id/register",
+      name: "pet-register",
+      component: petRegister,
       meta: {
         requiresAuth: false
       }
