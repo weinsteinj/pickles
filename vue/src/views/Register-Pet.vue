ae7581a0b198a5bf232251a5d15027f2c07f5c38
Nicholas Soilis <nicholassoilis@gmail.com>
2022-08-09 13:42:16 -0400

    Fixed Register page

--- vue/src/views/Register-Pet.vue
+++ vue/src/views/Register-Pet.vue
@@ -11,7 +11,9 @@
       v-model="pet.name"
       required
       autofocus
-      />
+      >
+
+      
       <div></div>
 
       <label for="Species">Species:</label>
@@ -37,10 +39,43 @@
         
         <div></div>
 
+        <label for="petFixed">Fixed:</label>
+      <select id="petFixed" class="form-control" v-model="pet.isFixed" required>
+        <option value="1">Yes</option>
+        <option value="2">No</option>
+      </select>
+      <div></div>
+
+      <div></div>
+
+       <div></div>
+
+        <label for="petVac">Vaccinated:</label>
+      <select id="petVac" class="form-control" v-model="pet.hasVaccinations" required>
+        <option value="1">Yes</option>
+        <option value="2">No</option>
+      </select>
+      <div></div>
+
+        <label for="petSize">Size:</label>
+      <select id="petSize" class="form-control" v-model="pet.size" required>
+        <option value="1"> Up to 15 pounds</option>
+        <option value="2">15 - 30 pounds</option>
+        <option value="3">30 - 50 pounds</option>
+        <option value="4">50-90 pounds</option>
+        <option value="5">Bigger than 90 pounds</option>
+      </select>
+      <div></div>
+
+
         <label for="Personalities">Personalities:</label>
-       <multiselect id ="personality" v-model="value" :options="options" :close-on-select="false" multiple=true></multiselect>
+       <multiselect id="personality" v-model="value" :options="options" :close-on-select="false" :hide-selected="true" multiple=true></multiselect>
        
       <div></div>
+
+      <button id="selectBtn" type="submit">
+          Add Pet
+      </button>
         
       </form>
       
@@ -68,7 +103,7 @@ export default {
                 size:''   
             },
              value: '',
-            options: ['Timid', 'Tires Quickly', 'Independent', 'Playful', 'Toy Sharing', 'Confident', 'High Energy', 'Toy Possessive']
+        options: ['Timid', 'Tires Quickly', 'Independent', 'Playful', 'Toy Sharing', 'Confident', 'High Energy', 'Toy Possessive']
             
         }
     }
@@ -78,28 +113,86 @@ export default {
 
 <style src="vue-multiselect/dist/vue-multiselect.min.css">
  
+#personality {
+    width: 150px;
+}
+
 </style>
 
 <style>
- #tag {
-     width: 150px;
-     height: 25px;
-}
+ 
+
 
  #petName {
-    margin-left: 20px;
+   margin-left: 50px;
+   width: 170px;
+   margin-bottom: 0px;
 }
 
 #petSpecies {
     margin: 10px;
      width: 175px;
      height: 25px;
+     margin-left: 40px;
+
 }
 
 #petSex {
-    margin-left: 37px;
+    margin-left: 67px;
     width: 175px;
     height: 25px;
 }
+
+#birthDate {
+    margin-top: 10px;
+    margin-left: 34px;
+    margin-bottom: 10px;
+    width: 175px;
+}
+ #petFixed {
+     width: 175px;
+     height: 25px;
+     margin-left: 57px;
+    }
+
+#petVac { 
+     height: 25px;
+     width: 175px;
+     margin-top: 10px;
+     margin-left: 18px;
+    }
+
+
+#petSize {
+     width: 175px;
+     height: 25px;
+     margin-left: 63px;
+     margin-top: 10px;
+     margin-bottom: 10px;
+    }
+
+.multiselect {
+    width: 400px;
+    margin-bottom: 50px;;
+    margin-top: 10px;
+}
+
+#selectBtn {
+    margin-left: 95px;
+    width: 170px;
+}
+
+
+.multiselect {
+    width: 400px;
+    margin-bottom: 50px;;
+    margin-top: 10px;
+}
+
+#selectBtn {
+    margin-left: 95px;
+    width: 170px;
+}
+
 
 </style>
