23d92e958910a86f720bb5d2ae7e015b9d08e342
Bradley Mauger <bradleyamauger@gmail.com>
2022-08-09 15:18:25 -0400

    changed path for pet registry to 'pet'

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -55,7 +55,7 @@ const router = new Router({
       }
     },
     {
-      path: "/user/:user_id/register",
+      path: "/pet",
       name: "pet-register",
       component: petRegister,
       meta: {
