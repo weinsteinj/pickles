1ac0e187248924ac2ac0d60b1f22894cafbb325c
Nicholas Soilis <nicholassoilis@gmail.com>
2022-08-09 17:19:25 -0400

    Changed array of personalities to Strings

--- java/src/main/java/com/techelevator/model/Pet.java
+++ java/src/main/java/com/techelevator/model/Pet.java
@@ -11,13 +11,13 @@ public class Pet {
     private int species;
     private int sex;
     private Date birthDate;
-    private int[] personality;
+    private String[] personality;
     private boolean isFixed;
     private boolean hasVaccinations;
     private int size;
     private int userId;
 
-    public Pet(int petId, String name, int species, int sex, Date birthDate, int[] personality, boolean isFixed, boolean hasVaccinations, int size, int userId) {
+    public Pet(int petId, String name, int species, int sex, Date birthDate, String[] personality, boolean isFixed, boolean hasVaccinations, int size, int userId) {
         this.petId = petId;
         this.name = name;
         this.species = species;
@@ -74,11 +74,11 @@ public class Pet {
         this.birthDate = birthDate;
     }
 
-    public int[] getPersonality() {
+    public String[] getPersonality() {
         return personality;
     }
 
-    public void setPersonality(int[] personality) {
+    public void setPersonality(String[] personality) {
         this.personality = personality;
     }
 
