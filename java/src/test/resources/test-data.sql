f00b2e9c9cd49eb7d87e2d09cd383a5e0183ad9d
Caitlin Nagelson <cnagelson@gmail.com>
2022-08-11 14:47:12 -0400

    Tests in progress

--- java/src/test/resources/test-data.sql
+++ java/src/test/resources/test-data.sql
@@ -4,4 +4,9 @@ INSERT INTO users (first_name,last_name,username,password_hash,role,email_addres
 INSERT INTO users (first_name,last_name,username,password_hash,role,email_address) VALUES ('a','b','user2','user2','ROLE_USER','email@email.com');
 INSERT INTO users (first_name,last_name,username,password_hash,role,email_address) VALUES ('a','b','user3','user3','ROLE_USER','email@email.com');
 
+INSERT INTO pet (name, species, sex, birthday, is_fixed, is_vaccinated, size, user_id) VALUES ('pet1', 'dog', 'male', '2022-01-20', true, true, 2, 1);
+INSERT INTO pet (name, species, sex, birthday, is_fixed, is_vaccinated, size, user_id) VALUES ('pet2', 'dog', 'male', '2022-01-20', true, true, 2, 1);
+INSERT INTO pet (name, species, sex, birthday, is_fixed, is_vaccinated, size, user_id) VALUES ('pet3', 'dog', 'male', '2022-01-20', true, true, 2, 1);
+
+
 COMMIT TRANSACTION;
