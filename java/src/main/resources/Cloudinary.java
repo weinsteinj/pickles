9fc734f5ab1d944131dad5aad168354da90fa513
Aubrey Thompson <aubreyannthompson@gmail.com>
2022-08-11 11:17:04 -0400

    Cloudinary and Google maps APIs attempted

---
+++ java/src/main/resources/Cloudinary.java
@@ -0,0 +1,6 @@
+import com.cloudinary.*;
+...
+Cloudinary cloudinary = new Cloudinary(ObjectUtils.asMap(
+  "cloud_name", "picklepoints",
+  "api_key", "576356229922848",
+  "api_secret", "ohRGYseZ2p7IlOO_m0nIxeUvF_g"));
