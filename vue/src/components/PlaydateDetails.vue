f0047271a29d7669d65f7a5ff0d17c3191d3df62
Aubrey Thompson <aubreyannthompson@gmail.com>
2022-08-12 16:34:58 -0400

    playdates, trying to get pets by pet ids

--- vue/src/components/PlaydateDetails.vue
+++ vue/src/components/PlaydateDetails.vue
@@ -13,18 +13,30 @@ import playdateService from '@/services/playdateService.js';
 
 export default {
   name: "playdate-details",
-  created() {
-    playdateService.getById(this.$route.params.playdateId)
-      .then(response => {
-        this.playdate = response.data;
-        this.pets = this.playdate.petId;
-      })
-  },
   data() {
     return {
       playdate: {},
       pets: []
     }
+  },
+  created() {
+    playdateService.getById(this.$route.params.playdateId)
+      .then(response => {
+        this.playdate = response.data;
+      });
+  },
+  computed: {
+      getPetsForPlaydateDetails() {
+        console.log(this.playdate);
+        // for (var petid of this.playdate.petId) {
+        //     this.pets.push(this.$store.state.petArray.filter(x => {
+        //         x.petId === petid;
+        //     });
+        // }
+        return this.pets;
+      }
+   
   }
+  
 };
 </script>
