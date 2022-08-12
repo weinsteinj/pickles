6bda2f865bf0bad64c6231bdfd50c964a21daf6e
Aubrey Thompson <aubreyannthompson@gmail.com>
2022-08-12 15:21:26 -0400

    started playdate details page

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -8,6 +8,7 @@ import store from '../store/index'
 import petRegister from '../views/RegisterPet.vue'
 import GoogleMapView from '@/views/GoogleMapView.vue'
 import RegisterPlaydate from '@/views/RegisterPlaydate.vue'
+import PlaydateDetails from '@/views/PlaydateDetails.vue'
 
 Vue.use(Router)
 
@@ -76,6 +77,14 @@ const router = new Router({
       path: "/map",
       name: "map",
       component: GoogleMapView,
+      meta: {
+        requiresAuth: false
+      }
+    },
+    {
+      path: "/playdate/:playdateId",
+      name: "playdate-details",
+      component: PlaydateDetails,
       meta: {
         requiresAuth: false
       }
