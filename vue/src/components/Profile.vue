a9f30f714be6dc0f77bb5ee3f0f575790363aa7f
Aubrey Thompson <aubreyannthompson@gmail.com>
2022-08-14 15:21:52 -0400

    started profile page

--- vue/src/components/Profile.vue
+++ vue/src/components/Profile.vue
@@ -1,25 +1,36 @@
 <template>
   <div>
-      <h1>{{user.firstName}}'s Profile</h1>
+      <h1>Welcome to {{user.firstName}}'s Profile. Your pets:</h1>
+      <div v-for="pet in pets" v-bind:key="pet.petId" > {{pet.name}} </div>
   </div>
 </template>
 
 <script>
-import userService from '@/services/userService.js';
+//import userService from '@/services/userService.js';
+import petService from '@/services/petService.js';
 export default {
     name: 'profile-component',
 
     data() {
         return {
-            user: {}
+            user: {},
+            pets: []
         }
     },
 
     created() {
-        userService.getById(this.$route.params.userId).then((response) => {
-            this.user = response.data;
-        })
+        this.user = this.$store.state.user;
+        petService.getPetsByUserId(this.user.id).then((response) => {
+            this.pets = response.data;
+        });
+    },
+    mounted () {
+        
     }
+
+    //need:
+    // - this user's playdates
+    //to make this editable
 
 }
 </script>
