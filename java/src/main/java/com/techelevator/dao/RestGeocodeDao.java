dce1bd64b9a1f4ce9ac350b3b4510153816abfd4
Josh Weinstein <yamaotoko@gmail.com>
2022-09-04 20:04:28 -0400

    cleans up and simplifies Json parsing of geocode data at RestGeocodeDao

--- java/src/main/java/com/techelevator/dao/RestGeocodeDao.java
+++ java/src/main/java/com/techelevator/dao/RestGeocodeDao.java
@@ -3,9 +3,8 @@ package com.techelevator.dao;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.techelevator.model.Marker;
-import com.techelevator.model.Place;
-import com.techelevator.model.ZipCodeNotFoundException;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.techelevator.model.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -24,36 +23,23 @@ public class RestGeocodeDao implements GeocodeDao{
     @Override
     public Marker getGeocodeByZip(int zipCode) throws JsonProcessingException, ZipCodeNotFoundException {
         RestTemplate restTemplate = new RestTemplate();
-        Place place = new Place();
         String uri = "https://maps.googleapis.com/maps/api/geocode/json?address="
         + zipCode + "&key=AIzaSyCbMJBc_MS9mlIMfrzc96ZgSAZWe-ZvpnA";
-//        GeocodeResponse response = restTemplate.getForObject(uri, GeocodeResponse.class);
-         String stringPlace = restTemplate.getForObject(uri, String.class);
-         ObjectMapper mapper = new ObjectMapper();
-        JsonNode rootNode = mapper.readTree(stringPlace);
-        List<JsonNode> geoLatitudes = new ArrayList<>();
-        geoLatitudes = rootNode.findValues("lat");
-        if (geoLatitudes.isEmpty()) {
-            throw new ZipCodeNotFoundException();
-        }
-        BigDecimal lat = BigDecimal.valueOf(Double.parseDouble(String.valueOf(geoLatitudes.get(2))));
-        List<JsonNode> geoLongitudes = new ArrayList<>();
-        geoLongitudes = rootNode.findValues("lng");
+        String jsonString = restTemplate.getForObject(uri, String.class);
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode rootNode = mapper.readTree(jsonString);
+        JsonNode location = rootNode.findValue("location");
+        BigDecimal lat = location.get("lat").decimalValue();
+        BigDecimal lng = location.get("lng").decimalValue();
 
-        BigDecimal lng = BigDecimal.valueOf(Double.parseDouble(String.valueOf(geoLongitudes.get(2))));
-        Marker userMarker = new Marker(zipCode, lat, lng);
-
-//        GeocodeResponse response = new ObjectMapper().readValue(stringPlace, GeocodeResponse.class);
-        return userMarker;
+        return new Marker(zipCode, lat, lng);
     }
 //mapper.readValue(node.get("vendor").getTextValue(), Vendor.class);
 
     @Override
     public String getGeocodeTest15217() {
         RestTemplate restTemplate = new RestTemplate();
-        Place playPlace = new Place();
         String uri = "https://maps.googleapis.com/maps/api/geocode/json?address=15217&key=AIzaSyCbMJBc_MS9mlIMfrzc96ZgSAZWe-ZvpnA";
-
         String stringPlace = restTemplate.getForObject(uri, String.class);
 
         return stringPlace;
